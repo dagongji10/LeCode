8cb6927ed9cff749cf76270daf238b03e14f4c1e
dagongji10 <405612048@qq.com>
2019-08-26 23:57:31 +0800

    19/08/26

--- scripts/18.py
+++ scripts/18.py
@@ -1,29 +1,37 @@
 """
-Created on Sun Aug 25 16:24:23 2019
+Created on Mon Aug 26 21:44:09 2019
 @author: 短腿伊斯科22
 """
 
 # -*- coding: utf-8 -*-
 
 def fourSum(nums, target):
-    nums = sorted(nums)
+    if len(nums)<4:
+        return []
     
-    l0 = 0
-    r0 = len(nums)
-    while(r0-l0>2):
-        if 
-        a,d = nums[l0],nums[r0]
-        
-        l1 = l0+1
-        r1 = r0-1
-        while(l1<r1):
-            b,c = nums[l1],nums[r1]
-            _sum = a+b+c+d
-            if _sum==target:
-                pass
-            elif _sum<target:
-                pass
-            elif _sum>target:
-                pass
-        
-        
+    idxs = {}
+    ans = []
+    for i in range(len(nums)-1):
+        a = nums[i]
+        for j in range(i+1, len(nums)):
+            b = nums[j]
+            _sum = a+b
+            _diff = target-_sum
+            
+            if _diff in idxs:
+                for t in idxs[_diff]:
+                    m,n = t
+                    if m!=i and m!=j and n!=i and n!=j:
+                        tem = sorted([a, b, nums[m], nums[n]])
+                        ans.append(tem)
+            
+            if _sum in idxs:
+                idxs[_sum].append([i, j])
+            else:
+                idxs[_sum] = []
+                idxs[_sum].append([i, j])
+            
+    ans = [list(t) for t in set(tuple(_) for _ in ans)]
+    return ans
+            
+print(fourSum([-491,-487,-445,-436,-435,-429,-398,-385,-378,-370,-367,-353,-344,-325,-284,-279,-269,-262,-189,-182,-164,-152,-123,-118,-110,-64,-55,-48,-45,9,14,17,47,54,64,64,70,94,114,144,161,171,188,194,205,241,271,284,312,341,363,376,407,410,413,441,442,455,476,492], -1529))
